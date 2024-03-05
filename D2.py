9f3b50673602a817ae0ecd45f602dd7b852f78fa
Tejash Patel <76687011+tp6618@users.noreply.github.com>
2024-03-05 21:10:25 +0530

    Add files via upload

---
+++ D2.py
@@ -0,0 +1,20 @@
+# Variables In Python
+a=10
+b=10.1
+c=True
+d="Tejash"
+e=None
+print("Type is",type(a))
+print("Type is",type(b))
+print("Type is",type(c))
+print("Type is",type(d))
+print("Type is",type(e))
+
+list1=[10,20,30,"Tejash"]
+print(list1)
+
+tuple1=("Hi",100)
+print(tuple1)
+
+dict1={"Name":"Tejash", "Age":18}
+print(dict1)
