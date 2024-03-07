125c49fc23a83de4190f81d2136596e3472a1087
Tejash Patel <76687011+tp6618@users.noreply.github.com>
2024-03-07 22:07:57 +0530

    Add files via upload

---
+++ D3.py
@@ -0,0 +1,9 @@
+# Calculator
+a=10
+b=20
+print("The Addition of ",a,"+",b,"is",a+b)
+print("The Subtraction of ",a,"-",b,"is",a-b)
+print("The Multiplication of ",a,"*",b,"is",a*b)
+print("The Division of ",a,"/",b,"is",a/b)
+print("The Floor Division of ",a,"//",b,"is",a//b)
+print("The Modulo of ",a,"%",b,"is",a%b)
