d50f29c49c2ddbd9b3150c6c4b10438029f5cd15
Tejash Patel <76687011+tp6618@users.noreply.github.com>
2024-03-11 20:23:23 +0530

    Add files via upload

---
+++ D6.py
@@ -0,0 +1,17 @@
+#String
+
+Name="Tejash"
+Message=""" Hi
+my name is tejash
+
+this is multi line string 
+
+"""
+
+print(Name)
+print(Message)
+
+print(Name[1])
+
+for character in Name:
+    print(character)
